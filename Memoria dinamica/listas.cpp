b847b2ca62c6db6e42213690e09ff686c5ff2625
BhuntFTL <antonioduron234@gmail.com>
2025-05-08 08:49:43 -0600

    listas2

--- Memoria dinamica/listas.cpp
+++ Memoria dinamica/listas.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <string.h>
 using namespace std;
 
 //Declarar la estructura para al,acenar los nodos
@@ -15,6 +16,8 @@ de escribir (struct Nodo *elemento) para poder declarar una variable de tipo pun
 estructura, solo pondremos (Tlista elemento) y con eso resumimos el codigo*/
 
 void menu();
+Nodo capturaDatos();
+void insertarNodos(Tlista&, Nodo);
 
 int main(){
     menu();
@@ -22,6 +25,9 @@ int main(){
 
 void menu(){
     int opc;
+    Nodo aux; //Aux es una estructura, no un puntero
+    Tlista listaAlu; //Es un puntero de tipo nodo (struct Nodo *listaAlu)
+
     do{
         cout<<"LISTAS SIMPLES"<<endl;
         cout<<"Insertar nodos al inicio ....1"<<endl;
@@ -31,9 +37,35 @@ void menu(){
         cout<<"Tu opcion: ";
         cin>>opc;
         switch(opc){
-            case 1: break;
+            case 1: aux= capturaDatos();
+                    insertarNodos(listaAlu, aux);
+                    break;
             case 2: break;
             case 3: break;
+            case 4: break;
+            case 5: break;
         }
-    }
+    }while(opc!=6);
+}
+
+Nodo capturaDatos(){
+    Nodo reg;
+    cout<<"Nombre del alumno: ";
+    cin>>reg.nombre;
+    cout<<"Promedio del alumno: ";
+    cin>>reg.prom;
+    reg.ligaSig=NULL; //Este campo no se usa, por tanto se asigna como NULL o no se asigna nada
+    return reg;
+}
+
+void insertarNodos(Tlista &lista, Nodo aux){
+    Tlista alumno;
+    //Crear un elemento para almacenar en la lista
+    alumno= new(Nodo); //alumno= (Nodo*)malloc(1*sizeof(Nodo));
+    strcpy(alumno->nombre, aux.nombre);
+    alumno->prom = aux.prom;
+    alumno->ligaSig = lista;
+    lista = alumno;
+
+
 }
